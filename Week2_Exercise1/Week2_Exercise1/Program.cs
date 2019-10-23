b04cfef0df04ccf1de059f9d702b68cb9a485f03
KonstantinGeorgiev <56236081+KonstantinGeorgiev@users.noreply.github.com>
2019-10-23 17:19:30 +0300

    Add files via upload

---
+++ Week2_Exercise1/Week2_Exercise1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int b = int.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double d = double.Parse(Console.ReadLine());
+            double e = d + d * 0.2;
+            double f = b * 10;
+
+            double g = e * c * b + f;
+
+            Console.WriteLine("Total time needed to watch the {0} series is {1} minutes.", a, g);
+        }
+    }
+}
---
+++ Week2_Exercise2/Week2_Exercise2/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Film
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Film = Console.ReadLine();
+            string Type = Console.ReadLine();
+            int TicketsCount = int.Parse(Console.ReadLine());
+
+            if (Type == "Drink")
+            {
+                if (Film == "John Wick")
+                {
+                    int TicketPrice = 12;
+                    int Cost = TicketPrice * TicketsCount;
+                    Console.WriteLine("Your bill is {0:f2} leva.", Cost);
+                }
+                else if (Film == "Star Wars")
+                {
+                    int TicketPrice = 18;
+                    int Cost = TicketPrice * TicketsCount;
+                    if (TicketsCount >= 4)
+                    {
+                        double NewCost = Cost - (Cost * 0.3);
+                        Console.WriteLine("Your bill is {0:f2} leva.", NewCost);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your bill is {0:f2} leva.", Cost);
+                    }
+                }
+                else if (Film == "Jumanji")
+                {
+                    int TicketPrice = 9;
+                    int Cost = TicketPrice * TicketsCount;
+                    if (TicketsCount == 2)
+                    {
+                        double NewCost = Cost - (Cost * 0.15);
+                        Console.WriteLine("Your bill is {0:f2} leva.", NewCost);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your bill is {0:f2} leva.", Cost);
+                    }
+                }
+            }
+            else if (Type == "Popcorn")
+            {
+                if (Film == "John Wick")
+                {
+                    int TicketPrice = 15;
+                    int Cost = TicketPrice * TicketsCount;
+                    Console.WriteLine("Your bill is {0:f2} leva.", Cost);
+                }
+                else if (Film == "Star Wars")
+                {
+                    int TicketPrice = 25;
+                    int Cost = TicketPrice * TicketsCount;
+                    if (TicketsCount >= 4)
+                    {
+                        double NewCost = Cost - (Cost * 0.3);
+                        Console.WriteLine("Your bill is {0:f2} leva.", NewCost);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your bill is {0:f2} leva.", Cost);
+                    }
+                }
+                else if (Film == "Jumanji")
+                {
+                    int TicketPrice = 11;
+                    int Cost = TicketPrice * TicketsCount;
+                    if (TicketsCount == 2)
+                    {
+                        double NewCost = Cost - (Cost * 0.15);
+                        Console.WriteLine("Your bill is {0:f2} leva.", NewCost);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your bill is {0:f2} leva.", Cost);
+                    }
+                }
+            }
+            else if (Type == "Menu")
+            {
+                if (Film == "John Wick")
+                {
+                    int TicketPrice = 19;
+                    int Cost = TicketPrice * TicketsCount;
+                    Console.WriteLine("Your bill is {0:f2} leva.", Cost);
+                }
+                else if (Film == "Star Wars")
+                {
+                    int TicketPrice = 30;
+                    int Cost = TicketPrice * TicketsCount;
+                    if (TicketsCount >= 4)
+                    {
+                        double NewCost = Cost - (Cost * 0.3);
+                        Console.WriteLine("Your bill is {0:f2} leva.", NewCost);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your bill is {0:f2} leva.", Cost);
+                    }
+                }
+                else if (Film == "Jumanji")
+                {
+                    int TicketPrice = 14;
+                    int Cost = TicketPrice * TicketsCount;
+                    if (TicketsCount == 2)
+                    {
+                        double NewCost = Cost - (Cost * 0.15);
+                        Console.WriteLine("Your bill is {0:f2} leva.", NewCost);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your bill is {0:f2} leva.", Cost);
+                    }
+
+                }
+            }
+        }
+    }
+}
